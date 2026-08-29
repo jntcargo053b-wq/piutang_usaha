3b864e5dd0e5314ba270e5252257042f86759cc0
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-08-29 22:34:52 +0700

    Remove redundant sqflite test import

--- test/piutang_database_test.dart
+++ test/piutang_database_test.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:piutang_usaha/models/pelanggan.dart';
 import 'package:piutang_usaha/models/pembayaran.dart';
@@ -28,89 +27,3 @@ void main() {
     final file = File(path);
     if (await file.exists()) await file.delete();
   });
-
-  test('laporan memisahkan pembayaran periode', () async {
-    final pelangganId = await db.insertPelanggan(Pelanggan(nama: 'Test'));
-    final transaksiId = await db.insertTransaksi(
-      TransaksiKredit(
-        pelangganId: pelangganId,
-        tanggal: DateTime(2026, 8, 5),
-        nomorResi: 'R1',
-        namaPenerima: 'A',
-        kotaTujuan: 'Malang',
-        jumlah: 100000,
-      ),
-    );
-    await db.insertPembayaran(
-      Pembayaran(
-        transaksiId: transaksiId,
-        tanggal: DateTime(2026, 8, 10),
-        jumlah: 30000,
-      ),
-    );
-    await db.insertPembayaran(
-      Pembayaran(
-        transaksiId: transaksiId,
-        tanggal: DateTime(2026, 9, 10),
-        jumlah: 20000,
-      ),
-    );
-
-    final result = await db.getRekapPeriode(
-      dari: DateTime(2026, 8, 1),
-      sampai: DateTime(2026, 8, 31),
-    );
-
-    expect(result.single['total_dibayar'], 50000);
-    expect(result.single['dibayar_periode'], 30000);
-  });
-
-  test('overpayment ditolak', () async {
-    final pelangganId = await db.insertPelanggan(Pelanggan(nama: 'Test'));
-    final transaksiId = await db.insertTransaksi(
-      TransaksiKredit(
-        pelangganId: pelangganId,
-        tanggal: DateTime(2026, 8, 1),
-        nomorResi: 'R2',
-        namaPenerima: 'A',
-        kotaTujuan: 'Malang',
-        jumlah: 100000,
-      ),
-    );
-
-    expect(
-      () => db.insertPembayaran(
-        Pembayaran(
-          transaksiId: transaksiId,
-          tanggal: DateTime(2026, 8, 2),
-          jumlah: 100001,
-        ),
-      ),
-      throwsA(isA<ValidasiException>()),
-    );
-  });
-
-  test('aggregate saldo pelanggan benar', () async {
-    final pelangganId = await db.insertPelanggan(Pelanggan(nama: 'Test'));
-    final transaksiId = await db.insertTransaksi(
-      TransaksiKredit(
-        pelangganId: pelangganId,
-        tanggal: DateTime(2026, 8, 1),
-        nomorResi: 'R3',
-        namaPenerima: 'A',
-        kotaTujuan: 'Malang',
-        jumlah: 100000,
-      ),
-    );
-    await db.insertPembayaran(
-      Pembayaran(
-        transaksiId: transaksiId,
-        tanggal: DateTime(2026, 8, 2),
-        jumlah: 25000,
-      ),
-    );
-
-    final result = await db.getPelangganDenganSisa();
-    expect(result.single['sisa_piutang'], 75000);
-  });
-}
