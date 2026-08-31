53bd47f5013a346a6a61ff3a8597f8f844443d42
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-09-01 02:32:51 +0700

    Add cash and transfer payment method

--- lib/models/pembayaran.dart
+++ lib/models/pembayaran.dart
@@ -3,19 +3,26 @@ class Pembayaran {
   final int transaksiId;
   final DateTime tanggal;
   final int jumlah;
+  final String? metode;
   final String? keterangan;
 
-  Pembayaran({this.id, required this.transaksiId, required this.tanggal,
-    required this.jumlah, this.keterangan});
+  Pembayaran({this.id, required this.transaksiId, required this.tanggal, required this.jumlah, this.metode, this.keterangan});
 
   Map<String, dynamic> toMap() => {
-    'id': id, 'transaksi_id': transaksiId, 'tanggal': tanggal.toIso8601String(),
-    'jumlah': jumlah, 'keterangan': keterangan,
+    'id': id,
+    'transaksi_id': transaksiId,
+    'tanggal': tanggal.toIso8601String(),
+    'jumlah': jumlah,
+    'metode': metode,
+    'keterangan': keterangan,
   };
 
   factory Pembayaran.fromMap(Map<String, dynamic> map) => Pembayaran(
-    id: map['id'] as int?, transaksiId: map['transaksi_id'] as int,
+    id: map['id'] as int?,
+    transaksiId: map['transaksi_id'] as int,
     tanggal: DateTime.parse(map['tanggal'] as String),
-    jumlah: (map['jumlah'] as num).toInt(), keterangan: map['keterangan'] as String?,
+    jumlah: (map['jumlah'] as num).toInt(),
+    metode: map['metode'] as String?,
+    keterangan: map['keterangan'] as String?,
   );
 }
